e9b125a127fb673609aa31d20bb5eb95820e60d8
SVPopova <s.v.skvortsova@yandex.ru>
2020-10-06 22:49:12 +0400

    Updates

---
+++ precache-manifest.2457b41692aaf13558e0d883194086fd.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8024045f42cfa196bb4f912e0b9a32f",
+    "url": "/jalousie2/index.html"
+  },
+  {
+    "revision": "a32b3dc329299c03a61f",
+    "url": "/jalousie2/static/css/main.bacb506a.chunk.css"
+  },
+  {
+    "revision": "a00597bd9077beff1b08",
+    "url": "/jalousie2/static/js/2.4ead18a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jalousie2/static/js/2.4ead18a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a32b3dc329299c03a61f",
+    "url": "/jalousie2/static/js/main.3041cff6.chunk.js"
+  },
+  {
+    "revision": "f3f0738baa8d2561b92d",
+    "url": "/jalousie2/static/js/runtime-main.84775935.js"
+  },
+  {
+    "revision": "259f263a56c0be1b909c98432952aa4c",
+    "url": "/jalousie2/static/media/babygirl.259f263a.jpg"
+  },
+  {
+    "revision": "4c2e4c219268c9e4c2d5e1a79576460f",
+    "url": "/jalousie2/static/media/boy.4c2e4c21.svg"
+  },
+  {
+    "revision": "283e980afa56eda2b8c99086e34fa0d2",
+    "url": "/jalousie2/static/media/deadline.283e980a.svg"
+  },
+  {
+    "revision": "180cefe679aec95ac7007580b26966ce",
+    "url": "/jalousie2/static/media/futurabookc-webfont.180cefe6.woff2"
+  },
+  {
+    "revision": "338c5a97d9f27c45a533b63e7bf274a0",
+    "url": "/jalousie2/static/media/futurabookc-webfont.338c5a97.eot"
+  },
+  {
+    "revision": "338c5a97d9f27c45a533b63e7bf274a0",
+    "url": "/jalousie2/static/media/futurabookc-webfont.338c5a97.eot@"
+  },
+  {
+    "revision": "63dc129eb99f7b2e19c736789a057c4f",
+    "url": "/jalousie2/static/media/futurabookc-webfont.63dc129e.svg"
+  },
+  {
+    "revision": "c4d15d68915719291809678f8b43a85d",
+    "url": "/jalousie2/static/media/futurabookc-webfont.c4d15d68.woff"
+  },
+  {
+    "revision": "eee18d8436e7411bed2990e0337ccf27",
+    "url": "/jalousie2/static/media/futurabookc-webfont.eee18d84.ttf"
+  },
+  {
+    "revision": "113b43c9f9b5db349a534618f49328e2",
+    "url": "/jalousie2/static/media/futurademic-webfont.113b43c9.woff"
+  },
+  {
+    "revision": "b158d1bd7007c48db00ccfc00d55b22d",
+    "url": "/jalousie2/static/media/futurademic-webfont.b158d1bd.svg"
+  },
+  {
+    "revision": "d532be329645284e5058deb661587670",
+    "url": "/jalousie2/static/media/futurademic-webfont.d532be32.eot"
+  },
+  {
+    "revision": "d532be329645284e5058deb661587670",
+    "url": "/jalousie2/static/media/futurademic-webfont.d532be32.eot@"
+  },
+  {
+    "revision": "ef11248527a4d76fe496f4931f81c468",
+    "url": "/jalousie2/static/media/futurademic-webfont.ef112485.ttf"
+  },
+  {
+    "revision": "f3fc88305c5ee7bd65cbebd52b8f0543",
+    "url": "/jalousie2/static/media/futurademic-webfont.f3fc8830.woff2"
+  },
+  {
+    "revision": "06c66f2a610f3b47c402e78aba03db71",
+    "url": "/jalousie2/static/media/futurapresspress-webfont.06c66f2a.woff"
+  },
+  {
+    "revision": "72e22a5284d9f0268889ebffb0686e0d",
+    "url": "/jalousie2/static/media/futurapresspress-webfont.72e22a52.svg"
+  },
+  {
+    "revision": "883460d37542c1334f08d318144f688d",
+    "url": "/jalousie2/static/media/futurapresspress-webfont.883460d3.woff2"
+  },
+  {
+    "revision": "a2f1b988a1f720545ea4f3fa750f4abb",
+    "url": "/jalousie2/static/media/futurapresspress-webfont.a2f1b988.eot"
+  },
+  {
+    "revision": "a2f1b988a1f720545ea4f3fa750f4abb",
+    "url": "/jalousie2/static/media/futurapresspress-webfont.a2f1b988.eot@"
+  },
+  {
+    "revision": "af4d45691716563a92785edce4fffe3a",
+    "url": "/jalousie2/static/media/futurapresspress-webfont.af4d4569.ttf"
+  },
+  {
+    "revision": "32ef4ebd12f44c5f2fb425a43513a8e1",
+    "url": "/jalousie2/static/media/girl.32ef4ebd.png"
+  },
+  {
+    "revision": "139d37c9d0a335b5e575704559eefef9",
+    "url": "/jalousie2/static/media/house.139d37c9.svg"
+  },
+  {
+    "revision": "e2a5dfdc0aa58520aa11a46630ca6b7e",
+    "url": "/jalousie2/static/media/offer.e2a5dfdc.svg"
+  },
+  {
+    "revision": "0cb84b7bedea74f3663665ad558522e8",
+    "url": "/jalousie2/static/media/safe.0cb84b7b.svg"
+  },
+  {
+    "revision": "25bbf494598fc454e13637fc8f71f071",
+    "url": "/jalousie2/static/media/trophy.25bbf494.svg"
+  },
+  {
+    "revision": "d83316c0fbb9b40e562077def907be6d",
+    "url": "/jalousie2/static/media/warranty.d83316c0.svg"
+  },
+  {
+    "revision": "5f16242668d7d9559daf6e45d251629a",
+    "url": "/jalousie2/static/media/zhaluzi1.5f162426.jpg"
+  },
+  {
+    "revision": "b83e2d08a6a3f1313e3f09705f7eb212",
+    "url": "/jalousie2/static/media/zhaluzi2.b83e2d08.jpg"
+  },
+  {
+    "revision": "c5da25c6ef2373495a5a1bc398e47473",
+    "url": "/jalousie2/static/media/zhaluzi3.c5da25c6.jpg"
+  },
+  {
+    "revision": "c5660dde27bd47cc86deb2826ecab09f",
+    "url": "/jalousie2/static/media/zhaluzi4.c5660dde.jpg"
+  },
+  {
+    "revision": "96400a4699d985b294005ad08c0015fd",
+    "url": "/jalousie2/static/media/zhaluzi6.96400a46.jpg"
+  },
+  {
+    "revision": "f4c4d56eb82869407f1a473aed9785bb",
+    "url": "/jalousie2/static/media/zhaluzi7.f4c4d56e.png"
+  },
+  {
+    "revision": "097c9ba799fa428acf943c0499458c5b",
+    "url": "/jalousie2/static/media/zhaluzi8.097c9ba7.jpg"
+  }
+]);
